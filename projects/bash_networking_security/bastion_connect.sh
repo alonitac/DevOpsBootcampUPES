208603100d8f08e0d4208311dfd3f87880d4a5b0
Deepanshu Rawat <83383871+deepanshu-rawat6@users.noreply.github.com>
2023-06-10 18:35:09 +0530

    project: completed solution 2

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+if [ -z "$KEY_PATH" ]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+
+public_instance_ip="$1"
+private_instance_ip="$2"
+command="$3"
+
+if [ -z "$public_instance_ip" ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+if [ -z "$private_instance_ip" ]; then
+   	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip"
+elif [ -z "$command" ]; then
+    	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "$KEY_PATH" ubuntu@"$private_instance_ip"
+else 
+	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "$KEY_PATH" ubuntu@"$private_instance_ip" "$command"
+fi

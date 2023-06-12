40d6f739172ec8e14ef6450d03395e3e4e2107a3
Abhishek Chamoli <AbhishekChamoli73@gmail.com>
2023-06-12 10:19:13 +0530

    SSH Bastion Host & SSH Keys Rotation

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+COMMAND=$3
+
+
+# Check if the KEY_PATH environment variable is set
+if [ -z "$KEY_PATH" ]; then
+  echo "KEY_PATH env var is expected"
+  exit 5
+fi
+
+# Check if the public instance IP is provided
+if [ -z "$1" ]; then
+  echo "Please provide bastion IP address"
+  exit 5
+fi
+
+# If both public and private instance IPs are provided, connect to the private instance via the public instance
+if [ -n "$2" ]; then
+  ssh -ti "$KEY_PATH" ubuntu@"$1" ssh -i "curr_key.pem" ubuntu@"$2" "$COMMAND"
+
+# Otherwise, connect to the public instance
+else
+  ssh -i "$KEY_PATH" ubuntu@"$1" "${@:2}"
+fi
+

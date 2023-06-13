52bcb5fd4fbfcf76d491bd9c869ccecf31d85650
royalkingzeher <111191629+royalkingzeher@users.noreply.github.com>
2023-06-13 22:37:28 +0530

    Update init.py

--- db_migration/init.py
+++ db_migration/init.py
@@ -5,6 +5,7 @@ import time
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
 collection = db["mycollection"]
+
 while True:
     data = {
         "dataId": str(uuid.uuid4()),

a290f792a8d89bf2b3e308215ccd9fc765c8e249
Gayatri <gayatri29mehta@gmail.com>
2023-06-14 22:26:04 +0530

    modified

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+# Check if KEY_PATH environment variable exists
+if [ -z "$KEY_PATH" ]; then
+  echo "Error: KEY_PATH environment variable is not set."
+  exit 5
+fi
+
+# Extract the filename from the key path
+key_filename=$(basename "$KEY_PATH")
+
+# SSH command to connect through the bastion (public instance) to the private instance
+ssh_command="ssh -o ProxyCommand=\"ssh -i $key_filename -W %h:%p ec2-user@<public_instance_ip>\" -i $KEY_PATH ec2-user@<private_instance_ip>"
+
+# Execute the SSH command
+eval "$ssh_command"

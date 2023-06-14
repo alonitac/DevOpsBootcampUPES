bc6b946215316eb6d19d93cf84aea4bf4d551b05
Tushar Bahuguna <111565432+2sharBahuguna@users.noreply.github.com>
2023-06-14 21:31:33 +0530

    Update bastion_connect.sh

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+if [ -z "$KEY_PATH" ]; then
+echo "Error, environamt variable not set"
+exit 5
+fi
+
+echo "Enter the instance: in user@public-instance-ip "
+read inst
+ssh -i "$KEY_PATH" "$inst" << EOF
+
+EOF
+

85204317e34fc76e74eec9475b93b89768ff0a30
Gayatri <gayatri29mehta@gmail.com>
2023-06-15 15:40:35 +0530

    pub-ec2-ip-added

--- projects/bash_networking_security/tlsHandshake.sh
+++ projects/bash_networking_security/tlsHandshake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PUBLIC_INSTANCE_IP=13.52.239.222
+
 # Step 1 - Send Client Hello request and parse Server Hello response
 client_hello='{
   "version": "1.3",
@@ -10,7 +12,7 @@ client_hello='{
   "message": "Client Hello"
 }'
 
-server_hello=$(curl -X POST -H "Content-Type: application/json" -d "$client_hello" http://localhost:8080/clienthello)
+server_hello=$(curl -X POST -H "Content-Type: application/json" -d "$client_hello" http://"$PUBLIC_INSTANCE_IP":8080/clienthello)
 version=$(echo "$server_hello" | jq -r '.version')
 cipher_suite=$(echo "$server_hello" | jq -r '.cipherSuite')
 session_id=$(echo "$server_hello" | jq -r '.sessionID')
@@ -36,7 +38,7 @@ key_exchange='{
   "sampleMessage": "Hi server, please encrypt me and send to client!"
 }'
 
-key_exchange_response=$(curl -X POST -H "Content-Type: application/json" -d "$key_exchange" http://localhost:8080/keyexchange)
+key_exchange_response=$(curl -X POST -H "Content-Type: application/json" -d "$key_exchange" http://"$PUBLIC_INSTANCE_IP":8080/keyexchange)
 encrypted_sample_message=$(echo "$key_exchange_response" | jq -r '.encryptedSampleMessage')
 
 # Step 5 - Decrypt and verify the sample message

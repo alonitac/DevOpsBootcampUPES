b737385ba9d18b3be73adbb41a511300cafac290
Antriksh <Antriksh>
2023-06-15 23:59:21 +0530

    Antriksh Final1

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [ -z "$KEY_PATH" ]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+public_instance_ip="$1"
+private_instance_ip="$2"
+command="$3"
+if [ -z "$public_instance_ip" ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
 if [ -z "$private_instance_ip" ]; then
    	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip"
 elif [ -z "$command" ]; then

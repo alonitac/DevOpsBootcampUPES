5fdd96724f2ba1f595415b80a6f592543bee284c
Deepanshu Rawat <83383871+deepanshu-rawat6@users.noreply.github.com>
2023-06-15 21:11:46 +0530

    updated bastion connect and ssh_key_rotation

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -17,7 +17,7 @@ fi
 if [ -z "$private_instance_ip" ]; then
    	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip"
 elif [ -z "$command" ]; then
-    	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "$KEY_PATH" ubuntu@"$private_instance_ip"
+    ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "~/new_key" ubuntu@"$private_instance_ip"
 else 
-	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "$KEY_PATH" ubuntu@"$private_instance_ip" "$command"
+	ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "~/new_key" ubuntu@"$private_instance_ip" "$command"
 fi

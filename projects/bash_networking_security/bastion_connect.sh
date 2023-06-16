22b2450f24faea45a0e8d84273770dc937876771
Nizaul Rahman <76219204+nizrhm@users.noreply.github.com>
2023-06-16 10:32:41 +0530

    Updated bastion_connect.sh

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+# Check if KEY_PATH environment variable exists
+if [[ -z "$KEY_PATH" ]]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+# Check if the public instance IP address is provided
+if [[ $# -lt 1 ]]; then
+    echo "Please provide bastion IP address"
+    exit 1
+fi
+# Connect to the private instance using the public instance as a bastion host
+if [[ $# -eq 2 ]]; then
+    public_instance_ip=$1
+    private_instance_ip=$2
+
+    # Connect to the private instance via the bastion host
+    ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" ssh -t -t -i "/home/keys/nizrhm-ssh-keypair.pem" ubuntu@"$private_instance_ip"
+else
+    public_instance_ip=$1
+    # Connect to the public instance
+    ssh -i "$KEY_PATH" ubuntu@"$public_instance_ip" 
+fi

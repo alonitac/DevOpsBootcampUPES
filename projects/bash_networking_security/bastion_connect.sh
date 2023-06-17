b5d1ff498e31ec66c100496ec2b2e9a79ad11d36
royalkingzeher <111191629+royalkingzeher@users.noreply.github.com>
2023-06-17 10:50:54 +0530

    Update bastion_connect.sh

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 
 if [[ -z "$KEY_PATH" ]]; then
-  echo "Error: KEY_PATH environment variable is not set."
+
+  echo "KEY_PATH environment variable is not set!"
+
   exit 5
+
 fi
+
+
+
 
 if [[ $# -lt 1 ]]; then
+
   echo "KEY_PATH env var is expected"
-  echo "Please provide bastion IP address"
+
+  echo "Please provide Public Instance (Bastion) IP address"
+
   exit 5
+
 fi
 
-bastion_ip=$1
+
+
+
+public_ip=$1
+
 private_ip=$2
-command_to_run="${@:3}"
+
+command="${@:3}"
+
+
+
 
 if [[ -n "$private_ip" ]]; then
-  ssh -t -i "$KEY_PATH" ubuntu@"$bastion_ip" ssh -i "new_key" ubuntu@"$private_ip" "$command_to_run"
+
+  ssh -t -i "$KEY_PATH" ubuntu@"$public_ip" ssh -i "new_key" ubuntu@"$private_ip" "$command"
+
 else
-  ssh -i "$KEY_PATH" ubuntu@"$bastion_ip" "$command_to_run"
+
+  ssh -i "$KEY_PATH" ubuntu@"$public_ip" "$command"
+
 fi
